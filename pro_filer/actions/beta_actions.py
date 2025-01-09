14333c44d0a196833b4e0724b73bd716425225da
Sthéfani <sthefani_martinelli@hotmail.com>
2025-01-09 08:33:58 -0300

    Req 1 - corrects bug in func show_deepest_file.py

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,7 +5,9 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(
+            context["all_files"], key=lambda path: path.count("/")
+        )
         print(f"Deepest file: {deepest_file}")
 
 
